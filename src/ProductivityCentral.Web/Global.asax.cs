fab158c8614512afcb84c897430ab25f3e6205ca
Niranjan Singh <niranjan.singh@mind-infotech.com>
2022-03-24 22:57:23 +0530

    configured environment variable and Autofac resolver

--- src/ProductivityCentral.Web/Global.asax.cs
+++ src/ProductivityCentral.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac.Integration.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,10 +13,14 @@ namespace ProductivityCentral.Web
     {
         protected void Application_Start()
         {
+            //Set the variable to use int the log file path
+            System.Environment.SetEnvironmentVariable("OPCENT_BASEDIR", ProductivityCentral.Utils.PathHelper.GetRootedPath(), EnvironmentVariableTarget.Process);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //Set the resolver for the DI
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(AutofacConfig.Container));
         }
     }
 }
